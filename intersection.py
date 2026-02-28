9457939f19b80f8db7e7c40e4405ec2e1e009eba
Simon Edmunds <sjebkc@umsystem.edu>
2026-02-27 21:04:52 -0600

    Finishing up representations

--- intersection.py
+++ intersection.py
@@ -1,6 +1,11 @@
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import StrEnum
+
+class LightState(StrEnum):
+    RED = "Red"
+    YELLOW = "Yellow"
+    GREEN = "Green"
 
 @dataclass
 class Car:
@@ -9,15 +14,24 @@ class Car:
     wait_time: float
 
 @dataclass
+class TrafficSignal:
+    state: LightState = LightState.RED
+
+@dataclass
+class CrosswalkSignal:
+    state: LightState = LightState.RED
+
+@dataclass
 class Crosswalk:
+    light: CrosswalkSignal = CrosswalkSignal()
     people_waiting: int = 0
-    light_on: bool = False
     button_pressed: bool = False
 
 @dataclass
 class Street:
-    cars: list[Car] = None
+    cars: list[Car] = field(default_factory=list)
     crosswalk: Crosswalk | None = None
+    light: TrafficSignal = TrafficSignal()
 
 class Intersection:
     def __init__(self, streets: list[Street]):
